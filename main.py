2793c04b33eaa585d54053c7b51ba31ec9a0c88a
Jared Zondler <zman321@gmail.com>
2011-10-30 17:34:19 -0400

    added camera and accepting motion

--- main.py
+++ main.py
@@ -8,22 +8,35 @@ from direct.interval.IntervalGlobal import *  #for compound intervals
 from direct.task import Task         #for update fuctions
 import sys, math, random
 
+#our modules
+from PlayerBike import PlayerBike
+from Terrain import Terrain
+
 class World(DirectObject):
     def __init__(self):
-        camera.setPosHpr(0, -15, 7, 0, -15, 0)
         #load all the models
-        self.loadModels()
-    
-    def loadModels(self):
-        """loads initial models into the world"""
+        self.w_terrain = Terrain()
+        self.p_bike = PlayerBike()
         
-        #directly load the terrain and bike
-        self.bike = loader.loadModel("temp_bike.egg")
-        self.bike.reparentTo(render)
-        #self.bike.setScale(.005)
+        #disable mouse
+        base.disableMouse()
+        #parent the camera to the player bike and offset the initial location
+        camera.reparentTo(self.p_bike.bike)
+        camera.setZ(7)
+        camera.setP(-15)
+        camera.setY(-15)
         
-        self.terrain = loader.loadModel("temp_terrain.egg")
-        self.terrain.reparentTo(render)
+        #set up accept tasks
+        self.accept("escape", sys.exit)
+        self.accept("arrow_up", self.p_bike.setDirection, ["forward", 1])
+        self.accept("arrow_right", self.p_bike.setDirection, ["right", 1])
+        self.accept("arrow_left", self.p_bike.setDirection, ["left", 1])
+        self.accept("arrow_up-up", self.p_bike.setDirection, ["forward", 0])
+        self.accept("arrow_right-up", self.p_bike.setDirection, ["right", 0])
+        self.accept("arrow_left-up", self.p_bike.setDirection, ["left", 0])
+        
+        
+        
         
 w = World()
 run()
