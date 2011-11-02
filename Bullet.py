10ec0128263f56672f94b0fdf3a1ca12c6f13565
Jared Zondler <zman321@gmail.com>
2011-11-02 15:36:14 -0400

    bullets shoot properly. will add collision next

--- Bullet.py
+++ Bullet.py
@@ -10,28 +10,29 @@ import sys, math, random
 
 class Bullet(DirectObject):
     def __init__(self):
+        #create a constant speed and an overall bullet list to contain all bullets
         self.speed = .5
         self.bulletList = []
-        taskMgr.add(self.update, "bulletTask")
         self.prevTime = 0
         
     def createBullet(self, gun, bike):
+        #load the model and set the pos and H to the bike's
         self.bullet = loader.loadModel("temp_bullet.egg")
-        self.bullet.reparentTo(gun)
-        self.bullet.setPos(gun.getX(), gun.getY(), gun.getZ())
-        self.bullet.setH(gun.getH())
-        
+        self.bullet.setPos(bike.getX(), bike.getY(), bike.getZ())
+        self.bullet.setH(bike.getH())
+        self.bullet.reparentTo(render)
+        #add bullet to overall list
         self.bulletList.append(self.bullet)
         
     def update(self, task):
         elapsed = task.time - self.prevTime
+        #cycle through the bullet list and update the positions
         for bullet in self.bulletList:
-            bullet.reparentTo(render)
             angle = deg2Rad(bullet.getH())
             dist = self.speed * elapsed
             dx = dist * math.sin(angle)
             dy = dist * -math.cos(angle)
-            self.bullet.setPos(self.bullet.getX() - dx, self.bullet.getY() - dy, 0)
+            bullet.setPos(bullet.getX() - dx, bullet.getY() - dy, .5)
         self.prevTime = task.time
         return Task.cont
         
