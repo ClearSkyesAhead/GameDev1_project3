2658a980530dab710aabb774d9f75b9c9a89bd27
Jared Zondler <zman321@gmail.com>
2011-11-08 15:16:26 -0500

    gave main access to nodepath and created hp

--- EnemyBike.py
+++ EnemyBike.py
@@ -26,9 +26,10 @@ def visOut(event):
 class EnemyBike(Bike):
     def __init__(self, cTrav, cevent):
         #messenger.toggleVerbose()
-        Bike.__init__(self, cTrav)
+        self.Bike.__init__(self, cTrav)
         self.bike.setPos(0, 0, 10)
         self.initAI()
+        self.hp = 10
         
         frombikemask = BitMask32(0x10)
         intobikemask = BitMask32.allOff()
